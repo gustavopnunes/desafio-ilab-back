7a370d417ddb5ccd105076ecb59fe4657047ec49
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2022-03-22 17:45:56 -0300

    create project structure

---
+++ src/main/java/br/com/grupodois/desafioilab/DesafioIlabApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupodois.desafioilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioIlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioIlabApplication.class, args);
+	}
+
+}
