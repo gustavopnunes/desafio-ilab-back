363df07e451a8fda91ce14c25b2ee533f416f0de
Quezia <queziaamenezes@gmail.com>
2022-03-23 12:10:06 -0300

    segunda correção do nome de propriedades

--- src/main/java/br/com/grupodois/desafioilab/model/Orders.java
+++ src/main/java/br/com/grupodois/desafioilab/model/Orders.java
@@ -21,29 +21,29 @@ public class Orders {
 	private Long id;
 	
 	@Column(name = "order_date", nullable = false)
-	private Timestamp order_date;
+	private Timestamp orderDate;
 	
 	@Column(name = "order_value", nullable = false)
-	private Double order_value;
+	private Double orderValue;
 	
 	@Column(name = "order_status", length = 20, nullable = false)
-	private String order_status;
+	private String orderStatus;
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
-	private Client client_id;
+	private Client clientId;
 
 	public Orders() {
 		super();
 	}
 
-	public Orders(Long id, Timestamp order_date, Double order_value, String order_status, Client client_id) {
+	public Orders(Long id, Timestamp orderDate, Double orderValue, String orderStatus, Client clientId) {
 		super();
 		this.id = id;
-		this.order_date = order_date;
-		this.order_value = order_value;
-		this.order_status = order_status;
-		this.client_id = client_id;
+		this.orderDate = orderDate;
+		this.orderValue = orderValue;
+		this.orderStatus = orderStatus;
+		this.clientId = clientId;
 	}
 
 	public Long getId() {
@@ -54,36 +54,37 @@ public class Orders {
 		this.id = id;
 	}
 
-	public Timestamp getOrder_date() {
-		return order_date;
+	public Timestamp getOrderDate() {
+		return orderDate;
 	}
 
-	public void setOrder_date(Timestamp order_date) {
-		this.order_date = order_date;
+	public void setOrderDate(Timestamp orderDate) {
+		this.orderDate = orderDate;
 	}
 
-	public Double getOrder_value() {
-		return order_value;
+	public Double getOrderValue() {
+		return orderValue;
 	}
 
-	public void setOrder_value(Double order_value) {
-		this.order_value = order_value;
+	public void setOrderValue(Double orderValue) {
+		this.orderValue = orderValue;
 	}
 
-	public String getOrder_status() {
-		return order_status;
+	public String getOrderStatus() {
+		return orderStatus;
 	}
 
-	public void setOrder_status(String order_status) {
-		this.order_status = order_status;
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
 	}
 
-	public Client getClient_id() {
-		return client_id;
+	public Client getClientId() {
+		return clientId;
 	}
 
-	public void setClient_id(Client client_id) {
-		this.client_id = client_id;
+	public void setClientId(Client clientId) {
+		this.clientId = clientId;
 	}
+
 
 }
