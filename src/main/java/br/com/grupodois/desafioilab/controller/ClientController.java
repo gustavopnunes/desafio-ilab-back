af32bbf796d69c7625e5df35629611429c113f04
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2022-03-23 19:29:12 -0300

    Application deployed to heroku

--- src/main/java/br/com/grupodois/desafioilab/controller/ClientController.java
+++ src/main/java/br/com/grupodois/desafioilab/controller/ClientController.java
@@ -1,5 +1,13 @@
 package br.com.grupodois.desafioilab.controller;
 
-public class ClientController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class ClientController {
+	
+	@GetMapping("client")
+	public String teste() {
+		return "teste";
+	}
 }
