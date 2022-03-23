479de76c1541801bf71b6e4ddbfa23bbd0308dbf
Quezia <queziaamenezes@gmail.com>
2022-03-23 10:50:38 -0300

    mapeamento da entidade pedido

--- src/main/java/br/com/grupodois/desafioilab/model/Orders.java
+++ src/main/java/br/com/grupodois/desafioilab/model/Orders.java
@@ -1,5 +1,90 @@
 package br.com.grupodois.desafioilab.model;
 
-public class Orders {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Orders {
+	
+	@Id
+	@Column(name = "id", nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "order_date", nullable = false)
+	private Timestamp date;
+	
+	@Column(name = "order_value", nullable = false)
+	private Double value;
+	
+	@Column(name = "order_status", length = 20, nullable = false)
+	private String status;
+	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private Client client;
+
+	public Orders() {
+		super();
+	}
+
+	public Orders(Long id, Timestamp date, Double value, String status, Client client) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.value = value;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Timestamp getDate() {
+		return date;
+	}
+
+	public void setDate(Timestamp date) {
+		this.date = date;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+	
 }
