c5b65982bfdcc6449bdb0a323d9084fb13a57b8a
Jean Pierre <tavares1988@live.com>
2022-03-25 19:30:33 -0300

    changed end point to change Order status

--- src/main/java/br/com/grupodois/desafioilab/controller/TrackingStatusController.java
+++ src/main/java/br/com/grupodois/desafioilab/controller/TrackingStatusController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.grupodois.desafioilab.model.Orders;
 import br.com.grupodois.desafioilab.model.TrackingStatus;
+import br.com.grupodois.desafioilab.service.IOrdersService;
 import br.com.grupodois.desafioilab.service.ITrackingStatusService;
 
 @RestController
@@ -19,16 +21,31 @@ public class TrackingStatusController {
 	@Autowired
 	private ITrackingStatusService service;
 	
+	@Autowired
+	public IOrdersService orderService;
+	
 	@PostMapping
 	public ResponseEntity<?> createTrackingStatus (@RequestBody TrackingStatus novo) {
 		try {
-			novo = service.createTrackingStatus(novo);
+			Long orderId = novo.getOrder().getId();
+			Orders order = orderService.getOrderById(orderId);
 			
-			if (novo != null) {
-				return ResponseEntity.status(201).body(novo);
+			if (order != null) {
+				if (order.getOrderStatus().toLowerCase().equals("aberto")) {
+
+					order = orderService.updateOrder(order, "Em andamento");
+					
+					novo = service.createTrackingStatus(novo);
+					
+					if (novo != null) {
+						return ResponseEntity.status(201).body(novo);
+					}
+					
+					return ResponseEntity.status(404).body("Dados inválidos.");
+				}
+				return ResponseEntity.status(404).body("Pedido: "+ orderId + " não está disponível para entrega.");
 			}
-			
-			return ResponseEntity.status(404).body("Dados inválidos.");
+			return ResponseEntity.status(404).body("Produto: " + orderId + " não encontrado.");
 		} catch(Exception ex) {
 			ex.printStackTrace();
 			return ResponseEntity.status(500).body(ex.getMessage());
