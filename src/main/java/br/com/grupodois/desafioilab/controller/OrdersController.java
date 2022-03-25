8ddb3212161c812d3e0572d4f7b79550d941c15f
lisdrl <lisandre.darioli@gmail.com>
2022-03-25 12:40:48 -0300

    Update to implement service

--- src/main/java/br/com/grupodois/desafioilab/controller/OrdersController.java
+++ src/main/java/br/com/grupodois/desafioilab/controller/OrdersController.java
@@ -3,28 +3,28 @@ package br.com.grupodois.desafioilab.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.grupodois.desafioilab.dao.OrdersDAO;
 import br.com.grupodois.desafioilab.model.Orders;
+import br.com.grupodois.desafioilab.service.IOrdersService;
 
 @RestController
 @CrossOrigin("*")
+@RequestMapping("/orders")
 public class OrdersController {
 
 	@Autowired
-	private OrdersDAO dao;
+	public IOrdersService service;
 
-	@GetMapping("/orders")
-	public ResponseEntity<List<Orders>> getOrderByStatus(@RequestParam(name = "status") String status, @RequestParam(name = "items") int items) {
-    Pageable firstPage = PageRequest.of(0, items);
-    	List<Orders> response = dao.findAllByOrderStatus(status, firstPage);
+	@GetMapping
+	public ResponseEntity<List<Orders>> getAllOrdersByStatus(@RequestParam(name = "status") String status, @RequestParam(name = "items") int items) {
+		
+    	List<Orders> response = service.getOrderByStatus(status, items);
     	if (response.size() != 0) {
     		return ResponseEntity.ok(response);
     	}
