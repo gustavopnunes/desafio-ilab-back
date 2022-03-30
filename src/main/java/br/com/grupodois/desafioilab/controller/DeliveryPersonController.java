86f72acd3b2fa54c99f8d92adc3685aad367c36e
mayanna.porto <mayannaporto@hotmail.com>
2022-03-30 14:44:29 -0300

    swagger test

--- src/main/java/br/com/grupodois/desafioilab/controller/DeliveryPersonController.java
+++ src/main/java/br/com/grupodois/desafioilab/controller/DeliveryPersonController.java
@@ -11,18 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.grupodois.desafioilab.dao.DeliveryPersonDAO;
 import br.com.grupodois.desafioilab.model.DeliveryPerson;
 import br.com.grupodois.desafioilab.security.SystemCrypto;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @CrossOrigin("*")
-//@Api(value = "Entregador")
+@Api(value = "Entregador")
 public class DeliveryPersonController {
 	
 	@Autowired
 	DeliveryPersonDAO dao; 
 
-//	@ApiOperation(value = "Cadastro do Entregador")
+	@ApiOperation(value = "Cadastro do Entregador")
 	@PostMapping("/register")
 	public ResponseEntity<String> register(@RequestBody DeliveryPerson newUser) throws Exception { 
 		try { 
